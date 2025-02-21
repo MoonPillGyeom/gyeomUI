0ceda089c4719c1bd883393fc8a15d3398372cfa
MoonPillGyeom <ansvkf0820@naver.com>
2025-02-21 15:54:26 +0900

    fix: tailwind 삭제

--- src/components/TailwindTest/index.test.tsx
+++ src/components/TailwindTest/index.test.tsx
@@ -1,10 +1,9 @@
-import React from "react"
 import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react"
 import TailwindCSSTEST from "."
 
 describe('TailwindTest', () => {
-  const { container } = render(<TailwindCSSTEST />);
+  render(<TailwindCSSTEST />);
   const test = screen.getByText('test');
 expect(test).toBeInTheDocument();
 })
