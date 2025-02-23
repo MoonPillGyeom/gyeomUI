c68ffc4062b07f4f03a11538949546b99862087e
MoonPillGyeom <ansvkf0820@naver.com>
2025-02-23 20:37:07 +0900

    del: tailwindcss test delete

--- src/components/TailwindTest/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { render, screen } from "@testing-library/react"
-import TailwindCSSTEST from "."
-
-describe('TailwindTest', () => {
-  render(<TailwindCSSTEST />);
-  const test = screen.getByText('test');
-expect(test).toBeInTheDocument();
-})
