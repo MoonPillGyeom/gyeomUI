d7ea2fc4f188e8bc52ddd4abdb7ee5683c169487
MoonPillGyeom <ansvkf0820@naver.com>
2025-03-04 23:53:20 +0900

    refactor: Radio storybook 주석 제거

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import Radio from './Radio'; // 위에서 작성한 Radio 컴포넌트를 불러옵니다.
 const meta = {
@@ -41,47 +40,3 @@ export const defaultRadio: Story = {
     ),
   },
 };
-
-// 기본 예시: 기본 라디오 그룹과 라디오 아이템들
-// const Template: Story = (args) => {
-//   const [selectedValue, setSelectedValue] = useState<string | undefined>(undefined);
-
-//   return (
-//     <Radio {...args}>
-//       <Radio.Label>Choose your favorite color</Radio.Label>
-//       <Radio.Item value="red">Red</Radio.Item>
-//       <Radio.Item value="blue">Blue</Radio.Item>
-//       <Radio.Item value="green">Green</Radio.Item>
-//     </Radio>
-//   );
-// };
-
-// export const Default = Template.bind({});
-// Default.args = {
-//   name: 'color',
-//   defaultValue: 'blue',
-// };
-
-// // onChange 콜백을 이용한 예시
-// export const WithOnChange = Template.bind({});
-// WithOnChange.args = {
-//   name: 'color',
-//   defaultValue: 'red',
-//   onChange: (value: string) => {
-//     alert(`Selected value: ${value}`);
-//   },
-// };
-
-// // 기본값이 없는 예시
-// export const WithoutDefaultValue = Template.bind({});
-// WithoutDefaultValue.args = {
-//   name: 'color',
-//   children: (
-//     <>
-//       <Radio.Label>Choose your favorite fruit</Radio.Label>
-//       <Radio.Item value="apple">Apple</Radio.Item>
-//       <Radio.Item value="banana">Banana</Radio.Item>
-//       <Radio.Item value="cherry">Cherry</Radio.Item>
-//     </>
-//   ),
-// };
