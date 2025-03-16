2dbf0a1d289edec4d1adffadcd7cf6ec989a5299
MoonPillGyeom <ansvkf0820@naver.com>
2025-03-16 13:33:45 +0900

    feat: Carousel 뼈대

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import CarouselImg from "./CarouselImg";
 
-function Carousel() {
-  return (
-<div>Carousel</div>
-  )
+interface CarouselProps {
+  /** Carousel의 이미지 리스트 입니다. */
+  images: string[];
 }
 
-export default  Carousel;
+function Carousel({ images }: CarouselProps) {
+  console.log(images);
+
+  return (
+    <>
+      <div style={{width: '370px', height: '500px'}}>
+<div style={{display: 'flex', gap: '10px', overflow: "hidden"}}>
+      {images.map((img, index) => (
+        <CarouselImg src={img} alt={img} key={index} style={{ backgroundColor: 'red',  border: '2px solid green', borderRadius: '20px', width: '370px', height:'500px'}} />
+      ))}
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default Carousel;
